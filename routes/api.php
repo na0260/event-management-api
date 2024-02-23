471d519f376444a358b392a5bb3a5d04dcd62a4e
na0260 <samjr.008@gmail.com>
2024-02-23 18:37:33 +0600

    Event and attendee route added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\EventController;
+use App\Http\Controllers\Api\V1\AttendeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events',EventController::class);
+Route::apiResource('events.attendees',AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
