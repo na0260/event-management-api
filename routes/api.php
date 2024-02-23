1cf6c70fdc78507ae53b46085083e45877cdfaa1
na0260 <samjr.008@gmail.com>
2024-02-23 23:42:18 +0600

    login route added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\EventController;
 use App\Http\Controllers\Api\V1\AttendeeController;
+use App\Http\Controllers\Api\V1\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use App\Http\Controllers\Api\V1\AttendeeController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login',[AuthController::class, 'login']);
 
 Route::apiResource('events',EventController::class);
 Route::apiResource('events.attendees',AttendeeController::class)
