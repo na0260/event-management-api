c29708f5ddfaf611fbcbf53809f3a201aa40b2e1
na0260 <samjr.008@gmail.com>
2024-02-23 20:38:24 +0600

    fillable field added

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,7 +10,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Event extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        'name',
+        'description',
+        'start_time',
+        'end_time',
+        'user_id'
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
