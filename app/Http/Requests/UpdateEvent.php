5df3b375ed02f255c525ffa792457f016474b0a0
na0260 <samjr.008@gmail.com>
2024-02-23 21:49:12 +0600

    event update validation added

---
+++ app/Http/Requests/UpdateEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEvent extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'start_time' => ['sometimes', 'date'],
+            'end_time' => ['sometimes', 'date', 'after:start_time'],
+        ];
+    }
+}
